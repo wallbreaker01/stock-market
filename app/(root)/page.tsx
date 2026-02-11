2a73d30faa963aebed97cea7ae9dd679c472c2db
Sowmik Roy <107265135+wallbreaker01@users.noreply.github.com>
2026-02-11 19:21:44 +0600

    tradingwidget addition

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,59 @@
+import TradingViewWidget from '@/components/TradingViewWidget'
 import { Button } from '@/components/ui/button'
+import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from '@/lib/constants'
 
 
 const Home = () => {
+
+  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`
+
+
   return (
     <div className='flexmin-h-screen home-wrapper'>
-      Home
+      <section className='grid w-full gap-8 home-section'>
+        <div className='md:col-span-1 xl:col-span-1'>
+          <TradingViewWidget
+            title="Market Overview"
+            scriptUrl={`${scriptUrl}market-overview.js`}
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            height={600}
+            className='custom-chart'
+          />
+        </div>
+        <div className='h-full md:col-span-1 xl:col-span-2'>
+          <TradingViewWidget
+            title="Stock Heatmap"
+            scriptUrl={`${scriptUrl}stock-heatmap.js`}
+            config={HEATMAP_WIDGET_CONFIG}
+            height={600}
+            className='custom-chart'
+          />
+        </div>
+      </section>
+      <section className='grid w-full gap-8 home-section'>
+        <div className='h-full md:col-span-1 xl:col-span-1'>
+          <TradingViewWidget
+            title=""
+            scriptUrl={`${scriptUrl}timeline.js`}
+            config={TOP_STORIES_WIDGET_CONFIG}
+            height={600}
+            className='custom-chart'
+          />
+        </div>
+        <div className='h-full md:col-span-1 xl:col-span-2'>
+          <TradingViewWidget
+            title=""
+            scriptUrl={`${scriptUrl}market-quotes.js`}
+            config={MARKET_DATA_WIDGET_CONFIG}
+            height={600}
+            className='custom-chart'
+          />
+        </div>
+      </section>
     </div>
   )
 }
 
 export default Home
+
+
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -28,7 +28,7 @@ const UserDropdown = () => {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="flex items-centergap-3 text-gray-4 hover:text-yellow-500">
+                <Button variant="ghost" className="flex items-centergap-3 text-gray-400 hover:text-yellow-500">
                     <Avatar className="h-8 w-8">
                         <AvatarImage src="https://github.com/shadcn.png" />
                         <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
