bcad6c307d1c25942a1b7e0570c205470a3d2e3c
Sowmik Roy <107265135+wallbreaker01@users.noreply.github.com>
2026-02-13 18:50:50 +0600

    daily news fetching funtions-half

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,10 +1,10 @@
 import { inngest } from "@/lib/inngest/client";
-import { sendSingUpEmail } from "@/lib/inngest/functions";
+import { sendDailyNewsSummary, sendSignUpEmail } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [sendSingUpEmail],
+    functions: [sendSignUpEmail, sendDailyNewsSummary],
 })
--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,20 +1,20 @@
 import nodemailer from 'nodemailer';
-import { WELCOME_EMAIL_TEMPLATE } from './templates';
+import { NEWS_SUMMARY_EMAIL_TEMPLATE, WELCOME_EMAIL_TEMPLATE } from './templates';
 
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
-    auth:{
+    auth: {
         user: process.env.NODEMAILER_EMAIL!,
         pass: process.env.NODEMAILER_PASSWORD!,
-        
+
     }
 })
 
-export const sendWelcomeEmail = async ({email, name, intro}: WelcomeEmailData) => {
+export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData) => {
 
     const htmlTemplate = WELCOME_EMAIL_TEMPLATE.replace('{{name}}', name).replace('{{intro}}', intro);
 
-    const mailOptions={
+    const mailOptions = {
         from: `"Signalist" <${process.env.NODEMAILER_EMAIL}>`,
         to: email,
         subject: 'Welcome to Signalist! your stock market toolkit is ready!',
@@ -24,3 +24,21 @@ export const sendWelcomeEmail = async ({email, name, intro}: WelcomeEmailData) =
 
     await transporter.sendMail(mailOptions);
 }
+
+export const sendNewsSummaryEmail = async (
+    { email, date, newsContent }: { email: string; date: string; newsContent: string }
+): Promise<void> => {
+    const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
+        .replace('{{date}}', date)
+        .replace('{{newsContent}}', newsContent);
+
+    const mailOptions = {
+        from: `"Signalist News" <${process.env.NODEMAILER_EMAIL}>`,
+        to: email,
+        subject: `📈 Market News Summary Today - ${date}`,
+        text: `Today's market news summary from Signalist`,
+        html: htmlTemplate,
+    };
+
+    await transporter.sendMail(mailOptions);
+};
